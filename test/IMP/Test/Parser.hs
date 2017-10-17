5cba4e7fd7ec503d5b74144b6c230984c276d04a
Eugeniy Meshcheryakov <eugen@debian.org>
2017-10-17 03:07:19 +0200

    Add test cases for invalid string literals
    And improve test output.

--- test/IMP/Test/Parser.hs
+++ test/IMP/Test/Parser.hs
@@ -7,14 +7,31 @@ import Test.Tasty.HUnit
 import IMP.Parser
 import IMP.SourceLoc
 
-import Data.Text (Text)
+import Data.Text (Text, unpack)
 import Text.Megaparsec
+import Data.Char
+import Text.Printf
+
+-- | Escape invalid characters in string tst cases so they lookg better in the
+-- test report.
+escapeString :: Text -> String
+escapeString = concatMap escape . unpack
+ where
+  escape c
+    | isPrint c = [c]
+    | otherwise = printf "#{%02X}" $ fromEnum c
 
 validParseTest :: (Eq b, Show b) => Parser a -> (a -> b) -> Text -> b -> TestTree
 validParseTest p f input expected = testCase name $ Just expected @=? actual
  where
-  name = "for " ++ show expected
+  name = "for " ++ escapeString input
   actual = f <$> parseMaybe p input
+
+invalidParseTest :: (Eq a, Show a) => Parser a -> Text -> TestTree
+invalidParseTest p input = testCase name $ Nothing @=? actual
+ where
+  name = "for " ++ escapeString input
+  actual = parseMaybe p input
 
 stringLiteralTests :: [(Text, String)]
 stringLiteralTests =
@@ -31,10 +48,18 @@ stringLiteralTests =
      "Volume of cylinder (πr²h) = ")
   ]
 
+invalidStringLiteralTests :: [Text]
+invalidStringLiteralTests =
+  [ "\"unterminated string"
+  , "\"string with embedded newline\n\""
+  , "\"string with tab\tcharacter\""
+  ]
+
 stringTests :: [TestTree]
-stringTests = map f stringLiteralTests
+stringTests = map f stringLiteralTests ++ map f' invalidStringLiteralTests
  where
   f (i, r) = validParseTest stringLiteral unLoc i r
+  f' = invalidParseTest stringLiteral
 
 tests :: TestTree
 tests = testGroup "Parser"
